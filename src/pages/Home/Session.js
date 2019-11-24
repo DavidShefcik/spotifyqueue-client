a5745aefdb9b7d5959a92bf576d1e53586460e66
David Shefcik <davidshefcik@gmail.com>
2019-11-23 20:53:58 -0600

    Session checking on home page. Added session home page.

---
+++ src/pages/Home/Session.js
@@ -0,0 +1,65 @@
+/*
+ * Author: David Shefcik
+ * Created: 11/23/19
+ * Project | File: Spotify Queue Client | src/pages/Home/Session.js
+ */
+
+// Module imports
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+
+// CSS imports
+import styles from './css/Session.css'
+
+// Component props
+import Button from '../../components/Button'
+
+// Component
+class Session extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: ''
+    }
+  }
+  render() {
+    console.log(this.props)
+    return (
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.textArea}>
+            <p className={styles.title}>Hello, {this.props.username}.</p>
+          </div>
+          <div>
+            <div>
+              <Link to="/queue" title="My Queue">
+                <Button text="My Queue" action={() => {}} />
+              </Link>
+            </div>
+            <div>
+              <Link to="/join" title="Join a Queue">
+                <Button text="Join a Queue" action={() => {}} />
+              </Link>
+            </div>
+            <div>
+              <Link to="/logout" title="Logout">
+                <Button text="Logout" action={() => {}} />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+// Map state to props
+const mapStateToProps = state => {
+  return {
+    username: state.user.user
+  }
+}
+
+// Export
+export default connect(mapStateToProps)(Session)
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,23 +6,53 @@
 
 // Module imports
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 // CSS imports
 import './css/Home.css'
 
 // Component imports
 import Default from './Default'
+import Session from './Session'
+import LoadingPage from '../../components/LoadingPage'
 
 // Component
 class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pageContent: <LoadingPage text="Please wait" />
+    }
+  }
+  componentDidMount() {
+    let token = this.props.token
+    let user = this.props.username
+
+    if (
+      Object.entries(token).length === 0 ||
+      Object.entries(user).length === 0
+    ) {
+      this.setState({
+        pageContent: <Default />
+      })
+    } else {
+      this.setState({
+        pageContent: <Session />
+      })
+    }
+  }
   render() {
-    return (
-      <div>
-        <Default />
-      </div>
-    )
+    return <div>{this.state.pageContent}</div>
+  }
+}
+
+// Map state to props
+const mapStateToProps = state => {
+  return {
+    username: state.user.user,
+    token: state.token.token
   }
 }
 
 // Export
-export default Home
+export default connect(mapStateToProps)(Home)
